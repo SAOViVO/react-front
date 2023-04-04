f2fb93334e79306496d55c0aa625b1cc585f0264
Dylan <dylan.gavilan32@gmail.com>
2023-04-04 14:42:35 -0300

    add adv, overflow

--- src/components/Advertisement.tsx
+++ src/components/Advertisement.tsx
@@ -1,8 +1,9 @@
+import adv from '../static/adv.gif'
 export const Advertisement = () => {
   return (
         <div className='w-full bg-[#56CCF2] font-poppins
                         h-32 flex items-center justify-center'>
-            AD al servicio pago
+                          <img src={adv} alt=""  className='w-full h-full'/>
         </div>
   )
 }
